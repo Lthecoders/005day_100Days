f31b0ebb2a23056d0a0fc34ccce9966298fceaeb
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-28 02:38:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# myName = input("What's your name?: ")
+# if myName == "Nikhil" or myName=="nikhil":
+#   print("\nwelcome Nikhil")
+#   print("wow let's get in hell")
+# else:
+#   print("\nwho on the earth are you")
+
+# catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+# if catsOrDogs == "cat":
+#   print("Meow")
+# else:
+#   print("Woof")
+
+# catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+# if catsOrDogs == "cat":
+#   print("Meow")
+# else:
+#   print("\nWoof")
+
+# drink = input("Do you prefer coffee or tea?")
+# if drink == "coffee":
+#   print("Tea is better.")
+# else:
+#   print("\nExcellent choice.")
+"which character are you challenge?-------------day 5 challlenge---"
+
+print("which character are you challenge?\n")
+
+
+q_1 = input("are you fast at running ? ")
+if q_1 == "yes" or q_1=="Yes":
+  print("you are flash\n")
+else:
+  print("you are not flash\n")
+
+q_2 = input("are you fast at shooting ? ")
+if q_2 == "yes" or q_2=="Yes":
+  print("you are simon rally GHOST from call of duty\n ")
+else:
+  print("you are not GHOST\n")
+
+q_3 = input("are you good in terms of technology ? ")
+if q_3 == "yes" or q_3=="Yes":
+  print("you are IRON MAN\n")
+else:
+  print("you are not IRON MAN\n")
+
+q_4 = input("do you love trees ? ")
+if q_4 == "yes" or q_4=="Yes":
+  print("you are GROOT\n")
+else:
+  print("you are not GROOT\n")
+
+q_5 = input("do you got bitten by a spider'😏😏? ")
+if q_5 == "yes" or q_5=="Yes":
+  print("you WILL BE AN SPIDER MAN SOON\n")
+else:
+  print("you are not SPIDER MAN\n")
+
+print("great play no use you power wisely")
